b96c3767eeee06e3f910920d5cdba92c9ec1d094
IonUdvas <udv.ion8679@gmail.com>
2022-07-03 13:50:55 +0530

    Blogs Update

---
+++ js/blog.js
@@ -0,0 +1,19 @@
+//Get the button
+var scrollButton = document.getElementById("scrollToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.visibility = "visible"
+  } else {
+    scrollButton.style.visibility = "hidden";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
